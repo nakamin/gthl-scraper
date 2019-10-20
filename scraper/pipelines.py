37212e8b5ae2ec5e13c09c5dda90a3e3693527ae
Anton Kaminsky <antonwizkam@gmail.com>
2019-10-20 19:25:22 -0400

    fix firebase pipeline

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -8,16 +8,13 @@ class FirestoreWriterPipeline(object):
         db = firestore.Client()
         root = db.collection("standings-snapshots")
         meta = root.document("meta")
-        last_write = meta.get("last_update").date()
+        last_write = meta.get().get("last_update").date()
         now = datetime.now()
         self.should_run = last_write != now.date()
         if self.should_run:
             self.new_snapshot = root.document()
             self.new_snapshot.set({"timestamp": now})
             meta.set({"last_update": now})
-
-    def close_spider(self, spider):
-        pass
 
     def process_item(self, item, spider):
         if self.should_run:
